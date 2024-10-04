94d8acca78e78a4e39c6b94102896386d6dcf9c9
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-10-04 08:48:39 -0700

    Add files via upload

---
+++ Chapter1,2/chap-five.js
@@ -0,0 +1,17 @@
+// Q1
+// var sum = 25 + 30;
+
+// alert(sum);
+
+// Q2
+// var num1 = 25;
+// var num2 = 5;
+
+// var sum = num1 + num2;
+
+// alert(sum);
+
+// Q3
+// var modulusResult = 27 % 4;
+
+// alert (modulusResult);
---
+++ Chapter1,2/chap-four.js
@@ -0,0 +1,25 @@
+// // Q5
+// var myAddress = "Karachi"
+
+// alert(myAddress)
+
+// // Q6
+// var userName = "Mariyam Sana \n";
+// var userEmail = "mariyamsanaarain@gmail.com \n";
+// var userPassword = "12345";
+
+// alert(userName + userEmail + userPassword);
+
+// // Q7
+// // var user address = "Malir karachi";
+
+// var useraddress = "Malir karachi";
+
+// alert(useraddress)
+
+// // Q8
+// var rose = "Floribundas"
+
+// alert(rose);
+
+// alert(Rose);
