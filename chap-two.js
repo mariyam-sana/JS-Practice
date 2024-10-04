a7795c69fc3ce41c115b73007974e9b59dad161e
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-10-04 08:52:03 -0700

    Add files via upload

---
+++ chap-two.js
@@ -0,0 +1,32 @@
+// Q1
+// var message = "Hello, world!";
+
+// alert(message);
+
+// Q2
+// var age;
+
+// age = 25;
+
+// alert(age);
+
+// Q3
+// var greeting = "Good morning!";
+
+// alert(greeting);
+
+// greeting = "Good evening!";
+
+// alert(greeting);
+
+// Q4
+// var favoriteColor = "blue";
+
+// alert("My favorite color is blue");
+
+// Q5
+// var age;
+
+// age = 25;
+
+// alert(age);
