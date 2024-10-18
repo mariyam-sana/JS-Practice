7abea056d3dbbe40e6efc4df2ed6e4ed9421485b
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-10-18 08:07:00 -0700

    Add files via upload

---
+++ chapter-eleven/script.js
@@ -0,0 +1,56 @@
+// Q1
+// const square = (num) => num * num;
+// console.log(square(5));
+
+// Q2
+// const even = (arr) => arr.filter(num => num % 2 === 0);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const evenNumbers = even(numbers);
+// console.log(evenNumbers);
+
+// Q3
+// const conStrings = (str1, str2) => str1 + str2;
+
+// const result = conStrings("Hello, ", "World!");
+// console.log(result); 
+
+// Q4
+// const findMax = (arr) => (arr);
+
+// const numbers = [3, 7, 2, 9, 5];
+// const maxNumber = findMax(numbers);
+// console.log(maxNumber);
+
+// Q5
+// const sumArray = numbers => numbers;
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(sumArray(numbers));
+
+// Q6
+// const sayHello = () => console.log("Hello World");
+
+// sayHello(); 
+
+// Q7
+// const square = (number) => number * number;
+
+// console.log(square(5));
+
+// Q8
+// const isPositive = (number) => number > 0;
+
+// console.log(isPositive(5));  
+// console.log(isPositive(-3));  
+// console.log(isPositive(0)); 
+
+// Q9
+// const getRandomNumber = () => Math.floor(Math.random() * 50) + 1;
+
+// console.log(getRandomNumber());
+
+// Q10
+// const convertToSeconds = (minutes) => minutes * 60;
+
+// console.log(convertToSeconds(5));
